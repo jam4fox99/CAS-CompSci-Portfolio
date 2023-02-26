6e4ef15ce661799c10aa9b5ef1ccd7e6420dc91e
jam4fox99 <jam4fox88@gmail.com>
2023-02-26 15:55:58 -0800

    Dad Jokes

---
+++ DadJokes/DadJokes/DadJokesApp.swift
@@ -0,0 +1,17 @@
+//
+//  DadJokesApp.swift
+//  DadJokes
+//
+//  Created by Jake Milken on 12/15/22.
+//
+
+import SwiftUI
+
+@main
+struct DadJokesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ DadJokes/DadJokes/Joke.swift
@@ -0,0 +1,14 @@
+//
+//  Joke.swift
+//  DadJokes
+//
+//  Created by Jake Milken on 12/15/22.
+//
+
+import Foundation
+
+
+struct Joke: Codable{
+    var id:String
+    var joke:String
+}
